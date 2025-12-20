e800c6da6cf5a991e8302f8c188f0cfed79c7f37
Anant Kinlekar <anantkinlekar18@gmail.com>
2025-12-20 12:16:04 +0530

    Requirement 6: Created SpecUtil to handle API request config and common API Assertions

--- src/test/java/com/api/tests/MasterAPIRequestTest.java
+++ src/test/java/com/api/tests/MasterAPIRequestTest.java
@@ -1,42 +1,24 @@
 package com.api.tests;
-
-import static com.api.utils.AuthTokenProvider.*;
-import io.restassured.http.ContentType;
+import static com.api.utils.SpecUtil.*;
 import static org.hamcrest.Matchers.*;
-
 import static io.restassured.module.jsv.JsonSchemaValidator.*;
 import org.testng.annotations.Test;
-
 import static com.api.constant.Role.*;
-
-import static com.api.utils.ConfigManager.*;
-
 import static io.restassured.RestAssured.*;
+
 
 public class MasterAPIRequestTest {
 
     @Test
     public void masterAPIRequestTest(){
         given()
-                .baseUri(getProperty("BASE_URI"))
-                .and()
-                .header("Authorization",getToken(FD))
-                .and()
-                .contentType("")
-                .accept(ContentType.ANY)
+                .spec(requestSpecWithAuth(FD))
                 .when()
                 .post("master")
                 .then()
-                .log().body()
-                .and()
-                .statusCode(200)
-                .and()
+                .spec(responseSpec_OK())
                 .body("message", equalTo("Success"))
-                .and()
                 .body("data", notNullValue())
-                .and()
-                .time(lessThan(1000L))
-                .and()
                 .body("data", hasKey("mst_oem"))
                 .body("data", hasKey("mst_model"))
                 .body("data", hasKey("mst_action_status"))
@@ -47,13 +29,10 @@ public class MasterAPIRequestTest {
                 .body("data", hasKey("mst_service_location"))
                 .body("data", hasKey("mst_problem"))
                 .body("data", hasKey("map_fst_pincode"))
-                .and()
                 .body("data.mst_oem.size()", equalTo(2))
                 .body("data.mst_model.size()", equalTo(3))
-                .and()
                 .body("data.mst_oem.id", everyItem(notNullValue()))
                 .body("data.mst_oem.name", everyItem(notNullValue()))
-                .and()
                 .body(matchesJsonSchemaInClasspath("response-schema/MasterAPIRequestResponseSchema.json"));
 
     }
@@ -61,15 +40,10 @@ public class MasterAPIRequestTest {
     @Test
     public void invalidTokenMasterAPIRequestTest(){
         given()
-                .baseUri(getProperty("BASE_URI"))
-                .and()
-                .header("Authorization","")
-                .and()
-                .contentType("")
-                .accept(ContentType.ANY)
+                .spec(requestSpec())
                 .when()
                 .post("master")
                 .then()
-                .statusCode(401);
+                .spec(responseSpec_TEXT(401));
     }
 }
---
+++ src/test/java/com/api/utils/SpecUtil.java
@@ -0,0 +1,106 @@
+package com.api.utils;
+import static com.api.utils.ConfigManager.*;
+import static com.api.utils.AuthTokenProvider.*;
+import com.api.constant.Role;
+import com.api.pojo.UserCredentials;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import static org.hamcrest.Matchers.*;
+
+public class SpecUtil {
+
+    // GET and DEL
+    public static RequestSpecification requestSpec(){
+
+        RequestSpecification request =new RequestSpecBuilder()
+                .setBaseUri(getProperty("BASE_URI"))
+                .and()
+                .setContentType(ContentType.JSON)
+                .and()
+                .setAccept(ContentType.JSON)
+                .and()
+                .log(LogDetail.METHOD)
+                .log(LogDetail.URI)
+                .log(LogDetail.HEADERS)
+                .log(LogDetail.BODY)
+                .build();
+
+        return request;
+
+    }
+
+    public static RequestSpecification requestSpec(Object payload){
+
+        RequestSpecification request =new RequestSpecBuilder()
+                .setBaseUri(getProperty("BASE_URI"))
+                .and()
+                .setContentType(ContentType.JSON)
+                .and()
+                .setAccept(ContentType.JSON)
+                .and()
+                .setBody(payload)
+                .log(LogDetail.METHOD)
+                .log(LogDetail.URI)
+                .log(LogDetail.HEADERS)
+                .log(LogDetail.BODY)
+                .build();
+
+        return request;
+
+    }
+
+    public static RequestSpecification requestSpecWithAuth(Role role){
+        RequestSpecification request =new RequestSpecBuilder()
+                .setBaseUri(getProperty("BASE_URI"))
+                .and()
+                .setContentType(ContentType.JSON)
+                .and()
+                .setAccept(ContentType.JSON)
+                .and()
+                .addHeader("Authorization", getToken(role))
+                .log(LogDetail.METHOD)
+                .log(LogDetail.URI)
+                .log(LogDetail.HEADERS)
+                .log(LogDetail.BODY)
+                .build();
+
+        return request;
+    }
+
+    public static ResponseSpecification responseSpec_OK(){
+        ResponseSpecification response =new ResponseSpecBuilder()
+                .expectContentType(ContentType.JSON)
+                .expectStatusCode(200)
+                .expectResponseTime(lessThan(1000L))
+                .log(LogDetail.ALL)
+                .build();
+
+        return response;
+    }
+
+    public static ResponseSpecification responseSpec_JSON(int statusCode){
+        ResponseSpecification response =new ResponseSpecBuilder()
+                .expectContentType(ContentType.JSON)
+                .expectStatusCode(statusCode)
+                .expectResponseTime(lessThan(1000L))
+                .log(LogDetail.ALL)
+                .build();
+
+        return response;
+    }
+
+    //NOT CHECKING CONTENT TYPE IN THIS METHOD
+    public static ResponseSpecification responseSpec_TEXT(int statusCode){
+        ResponseSpecification response =new ResponseSpecBuilder()
+                .expectStatusCode(statusCode)
+                .expectResponseTime(lessThan(1000L))
+                .log(LogDetail.ALL)
+                .build();
+
+        return response;
+    }
+}
