12afbac4878fe43fbcb4027bb5d5936030507fd6
Anant Kinlekar <anantkinlekar18@gmail.com>
2025-12-20 15:38:30 +0530

    Requirement 7: Create JOB API Test Created POJO Classes respective payload in com.api.pojo. And also introduced basic test using spec util

--- src/test/java/com/api/tests/CreateJobAPITest.java
+++ src/test/java/com/api/tests/CreateJobAPITest.java
@@ -3,6 +3,8 @@ import static io.restassured.RestAssured.*;
 import static com.api.constant.Role.*;
 import com.api.pojo.*;
 import static com.api.utils.SpecUtil.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
+import static org.hamcrest.Matchers.*;
 import org.testng.annotations.Test;
 
 
@@ -12,7 +14,7 @@ public class CreateJobAPITest {
     public void createJobAPITTest() {
         Customer customer = new Customer("Anant", "Kinlekar", "7995924124", "", "anantkinlekar18@gmail.com", "");
         CustomerAddress customerAddress = new CustomerAddress("602", "Vasavi Arcade", "Munneshwar Temple Road", "ECC Road", "Paatandur Agrahara", "416410", "India", "Maharashtra");
-        CustomerProduct customerProduct = new CustomerProduct("2025-04-06T18:30:00.000Z", "15785930780289", "15785930780289", "15785930780289", "2025-04-06T18:30:00.000Z", 1, 1);
+        CustomerProduct customerProduct = new CustomerProduct("2025-04-06T18:30:00.000Z", "23685930780289", "23685930780289", "23685930780289", "2025-04-06T18:30:00.000Z", 1, 1);
         Problems problems = new Problems(1, "Battery issue");
         Problems[] problemsArray = new Problems[1];
         problemsArray[0] = problems;
@@ -32,7 +34,14 @@ public class CreateJobAPITest {
                 .when()
                 .post("/job/create")
                 .then()
-                .spec(responseSpec_OK());
+                .spec(responseSpec_OK())
+                .body(matchesJsonSchemaInClasspath("response-schema/CreateJobAPIResponseSchema.json"))
+                .body("message", equalTo("Job created successfully. "))
+                .body("data.mst_service_location_id", equalTo(1))
+                .body("data.mst_platform_id", equalTo(2))
+                .body("data.mst_warrenty_status_id", equalTo(1))
+                .body("data.mst_oem_id", equalTo(1))
+                .body("data.job_number", startsWith("JOB_"));
 
     }
 }
