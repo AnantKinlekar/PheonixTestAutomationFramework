f98f03b752717cca00d1e7fa42d7b6cf2fe752fe
Anant Kinlekar <anantkinlekar18@gmail.com>
2026-01-09 09:27:25 +0530

    Created Auth Service Class to maintain the login api at once place

---
+++ src/test/java/com/api/services/AuthService.java
@@ -0,0 +1,20 @@
+package com.api.services;
+
+import com.api.request.model.UserCredentials;
+import io.restassured.response.Response;
+
+import static com.api.utils.SpecUtil.requestSpec;
+import static io.restassured.RestAssured.given;
+
+public class AuthService {
+    //it will hold the APIs that belong to the Auth
+    private static final String LOGIN_ENDPOINT = "login";
+
+    public Response login(UserCredentials userCredentials) {
+        Response response = given()
+                .spec(requestSpec(userCredentials))
+                .when()
+                .post(LOGIN_ENDPOINT);
+        return response;
+    }
+}
--- src/test/java/com/api/tests/LoginAPITest.java
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -1,27 +1,33 @@
 package com.api.tests;
+
 import com.api.request.model.UserCredentials;
+
 import static com.api.utils.SpecUtil.*;
 import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
+import com.api.services.AuthService;
+import io.restassured.response.Response;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 import java.io.IOException;
+
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
+
 public class LoginAPITest {
     private UserCredentials userCredentials;
+    private AuthService authService;
 
     @BeforeMethod(description = "Create the payload for the Login API")
-    public void setup(){
-         userCredentials = new UserCredentials("iamfd", "password");
+    public void setup() {
+        userCredentials = new UserCredentials("iamfd", "password");
+        authService = new AuthService();
     }
 
     @Test(description = "Verifying if Login API is working for user: Front Desk", groups = {"api", "regression", "smoke"})
     public void loginAPITest() {
-        given()
-                .spec(requestSpec(userCredentials))
-                .when()
-                .post("/login")
+        authService.login(userCredentials)
                 .then()
                 .spec(responseSpec_OK())
                 .body("message", equalTo("Success"))
